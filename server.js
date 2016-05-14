5b7d8e7f0114599a3f4b3ea91869232c88c9b686
jvollherbst <jasmine.cardoza@gmail.com>
2016-05-14 15:38:11 -0400

    able to pull from weather and geolocation api. able to access lat and lon values

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 'use strict'
 
 require('dotenv').config();
-const express    = require('express');
-const logger     = require('morgan');
-const bodyParser = require('body-parser');
-const path       = require('path');
-const request    = require('request');
-const db         = require('./db/pg');
-const moment     = require('moment');
+const express     = require('express');
+const logger      = require('morgan');
+const bodyParser  = require('body-parser');
+const path        = require('path');
+const request     = require('request');
+const db          = require('./db/pg');
+const moment      = require('moment');
+const app         = express();
+const api_key     = process.env.API_KEY;
 // moment().format();
 
-const app        = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -20,19 +21,31 @@ app.use(express.static(path.join(__dirname, './public/')));
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
-// routes
-// app.use('/users', require(path.join(__dirname, '/routes/users')));
 
-// app.get('https://api.forecast.io/forecast/darkskyapi',  (req, res) => {
-//   res.render('index')
-// });
+app.get('/weather',  (req, res) => {
+  var query = req.query.location;
+  request({ url: api_key + query, json: true },
+    function(err, apires, body) {
+      console.log(query);
+      res.send(body)
+    })
+});
+
+app.get('/geo', (req, res) => {
+  request({ url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + req.query.address + 'key=' + process.env.GEOKEY,
+  json: true },
+    function(err, apires, body) {
+      res.send(body)
+      console.log(body.results[0].geometry.location.lat);
+    })
+});
 
 app.get('/', (req, res) => {
   res.render('index')
 });
 
-
+// app.use('/users', require(path.join(__dirname, '/routes/users')));
 // app.use('/', require(path.join(__dirname, '/routes/')));
-const port = process.env.PORT || 3000;
-const server = app.listen(port, function( ){
-});
+
+const port   = process.env.PORT || 3000;
+const server = app.listen(port, function( ){});
