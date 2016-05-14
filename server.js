4bd4f86720648ac697422993261490209ee212c3
jvollherbst <jasmine.cardoza@gmail.com>
2016-05-14 18:08:51 -0400

    location values from geocode can pass to weather api and render correct results

--- server.js
+++ server.js
@@ -22,19 +22,25 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 
 
-app.get('/weather', (req, res) => {
-  var query = req.query.location;
-  request({ url: api_key + query, json: true },
-    function(err, apires, body) {
-      res.send(body)
-    })
-});
+// app.get('/weather', (req, res) => {
+//   var query = req.query.location;
+//   request({ url: api_key + query, json: true },
+//     function(err, apires, body) {
+//       res.send(body)
+//     })
+// });
 
 app.get('/geo', (req, res) => {
   request({ url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + req.query.address + 'key=' + process.env.GEOKEY,
   json: true },
     function(err, apires, body) {
-      res.send(body)
+      var query = [body.results[0].geometry.location.lat, body.results[0].geometry.location.lng];
+      request({ url: api_key + query, json: true },
+        function(err, apires, body) {
+          res.send(body)
+        })
+
+      // res.send(body)
       // console.log(body.results[0].geometry.location.lat);
     })
 });
