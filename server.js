0f65a15df71bb80968f1233e67f7e0fdcfc7d97a
jvollherbst <jasmine.cardoza@gmail.com>
2016-05-18 20:22:48 -0400

    resolving issue with app crashing on deployment

--- server.js
+++ server.js
@@ -73,5 +73,5 @@ app.get('/', (req, res) => {
 
 app.use('/users', require(path.join(__dirname, '/routes/users')));
 
-const port   = process.env.PORT || 3000;
+const port   = process.env.PORT;
 const server = app.listen(port, function( ){});
