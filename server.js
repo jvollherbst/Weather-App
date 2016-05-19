48cc84791552731c7a64831bae651c16ae078038
jvollherbst <jasmine.cardoza@gmail.com>
2016-05-18 20:41:15 -0400

    --

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(session({
     varring : connectionString,
     tableName : 'session'
   }),
-  secret: process.env.SECRET,
+  secret: 's',
   resave: false,
   saveUninitialized: false,
   cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }
